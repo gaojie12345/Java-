d7dae6e1d093ca9ac064a8272759b642663716f8
gaojie12345 <1768916170@qq.com>
2019-08-24 22:34:43 +0800

    人员管理

---
+++ Projects/src/main/java/com/hrms/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.hrms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "/hrms")
+public class MainController {
+
+
+}
---
+++ Projects/src/main/java/com/hrms/service/EmployeeService.java
@@ -0,0 +1,35 @@
+package com.hrms.service;
+
+import com.hrms.bean.Employee;
+import com.hrms.mapper.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    public int getEmpCount(){
+        return employeeMapper.countEmps();
+    }
+    public List<Employee> getEmpList(Integer offset, Integer limit){
+        return employeeMapper.selectByLimitAndOffset(offset, limit);
+    }
+    public Employee getEmpById(Integer empId){
+        return employeeMapper.selectOneById(empId);
+    }
+    public Employee getEmpByName(String empName){return employeeMapper.selectOneByName(empName);};
+    public int updateEmpById(Integer empId, Employee employee){return employeeMapper.updateOneById(empId, employee);}
+    public int deleteEmpById(Integer empId){
+        return employeeMapper.deleteOneById(empId);
+    }
+    public int addEmp(Employee employee){
+        return employeeMapper.insertOne(employee);
+    }
+
+
+}
